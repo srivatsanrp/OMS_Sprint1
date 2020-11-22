740640df5f0deeff9308c9a14444ada2c631c869
Mohamed Inamul Hasan <45665586+inamulhasan770@users.noreply.github.com>
2020-11-22 21:39:52 +0530

    Add files via upload

---
+++ Exception/BillNotFoundException.java
@@ -0,0 +1,7 @@
+package com.cg.oms.exception;
+
+public class BillNotFoundException extends Exception{
+	public BillNotFoundException(String msg) {
+		super(msg);
+	}
+}
---
+++ Exception/NullValueException.java
@@ -0,0 +1,7 @@
+package com.cg.oms.exception;
+
+public class NullValueException extends Exception {
+public NullValueException(String msg) {
+	super(msg);
+}
+}
