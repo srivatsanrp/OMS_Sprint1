dba24b79ea51f11930a3ad5204e5ac918836f1b9
Mohamed Inamul Hasan <45665586+inamulhasan770@users.noreply.github.com>
2020-11-22 21:42:10 +0530

    Add files via upload

---
+++ Dao/JPAUtil.java
@@ -0,0 +1,35 @@
+package com.cg.oms.dao;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JPAUtil
+{
+
+ 
+
+    private static final String PERSISTENCE_UNIT_NAME = "oms_sprint";
+
+ 
+
+    private static EntityManagerFactory factory;
+    private static EntityManager entityManager;
+    static
+    {
+        factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+    }
+
+ 
+
+    public static EntityManager getEntityManager()
+    {
+        if (entityManager == null || !entityManager.isOpen())
+        {
+            entityManager = factory.createEntityManager();
+        }
+        return entityManager;
+    }
+
+ 
+
+}
